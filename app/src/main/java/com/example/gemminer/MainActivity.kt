6ca0a0dbc9220d1fcaad95ea75c8a28b1e73ea2c
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2022-06-02 01:19:48 +0900

    Implement Login and Register

--- app/src/main/java/com/example/gemminer/MainActivity.kt
+++ app/src/main/java/com/example/gemminer/MainActivity.kt
@@ -2,26 +2,43 @@ package com.example.gemminer
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gemminer.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var loginBinding: ActivityMainBinding
+    var DB:DBHelper?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         loginBinding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(loginBinding.root)
+        DB = DBHelper(this)
 
-
-        loginBinding.registerBtn.setOnClickListener {
-            val RegisterIntent = Intent(this@MainActivity, RegisterActivity::class.java)
-            startActivity(RegisterIntent)
+        loginBinding.loginBtn!!.setOnClickListener {
+            val user = loginBinding.nickname!!.text.toString()
+            val pass = loginBinding.password!!.text.toString()
+            if (user ==""|| pass=="") Toast.makeText(
+                this@MainActivity,
+                "회원정보를 전부 입력해주세요",
+                Toast.LENGTH_SHORT
+            ).show() else{
+                val checkUserpass = DB!!.checkUserpass(user, pass)
+                if (checkUserpass ==true){
+                    Toast.makeText(this@MainActivity, "로그인 되었습니다.", Toast.LENGTH_SHORT)
+                        .show()
+                    val intent = Intent(applicationContext, HomeActivity::class.java)
+                    startActivity(intent)
+                }
+                else {
+                    Toast.makeText(this@MainActivity, "회원정보가 존재하지 않습니다.", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
         }
-
-        loginBinding.loginBtn.setOnClickListener {
-            val loginIntent = Intent(this@MainActivity, HomeActivity::class.java)
+        loginBinding.registerBtn.setOnClickListener {
+            val loginIntent = Intent(this@MainActivity, RegisterActivity::class.java)
             startActivity(loginIntent)
         }
     }
--- app/src/main/java/com/example/gemminer/RegisterActivity.kt
+++ app/src/main/java/com/example/gemminer/RegisterActivity.kt
@@ -3,19 +3,59 @@ package com.example.gemminer
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import com.example.gemminer.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
     lateinit var registerBinding: ActivityRegisterBinding
+    var DB:DBHelper?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         registerBinding = ActivityRegisterBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(registerBinding.root)
+        DB = DBHelper(this)
 
         registerBinding.inputRegisterBtn.setOnClickListener {
-            val loginIntent = Intent(this, HomeActivity::class.java)
-            startActivity(loginIntent)
+            val user = registerBinding.inputNickname.text.toString()
+            val pass = registerBinding.inputPassword.text.toString()
+            val repass = registerBinding.reInputPassword.text.toString()
+            if (user == "" || pass == "" || repass == "") Toast.makeText(
+                this@RegisterActivity,
+                "회원정보를 전부 입력해주세요.",
+                Toast.LENGTH_SHORT
+            ).show() else {
+                if (pass == repass) {
+                    val checkUsername = DB!!.checkUsername(user)
+                    if (checkUsername == false) {
+                        val insert = DB!!.insertData(user, pass)
+                        if (insert == true) {
+                            Toast.makeText(
+                                this@RegisterActivity,
+                                "가입되었습니다.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            val intent = Intent(applicationContext, MainActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(
+                                this@RegisterActivity,
+                                "비밀번호가 일치하지 않습니다.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    } else {
+                        Toast.makeText(
+                            this@RegisterActivity,
+                            "이미 가입된 회원입니다.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                } else {
+                    Toast.makeText(this@RegisterActivity, "비밀번호가 일치하지 않습니디.", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
         }
+
     }
 }
